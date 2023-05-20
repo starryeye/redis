fd9e089a9f5589c58a14530a44730c5cc6f1a73e
starryeye <see2818@naver.com>
2023-05-21 01:10:34 +0900

    [starryeye, 2023.05.21] - redis + httpSession
    내용
    - httpSession 을 redis 로 관리한다.

---
+++ SessionStore/src/test/java/dev/practice/SessionStore/SessionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.SessionStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
