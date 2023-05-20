2903472e49ea361ad975300a960beaff03cd9c01
starryeye <see2818@naver.com>
2023-05-21 01:42:02 +0900

    [starryeye, 2023.05.21] - redis + caching init
    내용
    - redis + caching init

---
+++ RedisCaching/src/main/java/dev/practice/RedisCaching/RedisCachingApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.RedisCaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCachingApplication.class, args);
+	}
+
+}
