539cd1e7669ab2ebb254bb15fbaac776d411bb44
starryeye <see2818@naver.com>
2023-05-22 22:15:12 +0900

    [starryeye, 2023.05.22] - redis leaderboard init
    내용
    - leaderboard project init

---
+++ LeaderBoard/src/test/java/dev/practice/LeaderBoard/LeaderBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.LeaderBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
