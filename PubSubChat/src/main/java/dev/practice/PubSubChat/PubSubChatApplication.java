fbb74e1e4f8e6b9807f3bde4da69513cbf8f28f4
starryeye <see2818@naver.com>
2023-05-23 20:54:36 +0900

    [starryeye, 2023.05.23] - redis pub/sub init
    내용
    - redis pub/sub init

---
+++ PubSubChat/src/main/java/dev/practice/PubSubChat/PubSubChatApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.PubSubChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSubChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubSubChatApplication.class, args);
+	}
+
+}
