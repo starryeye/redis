56b2d7df5471dce6aab380ae2df971a30eca292a
starryeye <see2818@naver.com>
2023-05-25 21:03:39 +0900

    [starryeye, 2023.05.25] - redis sentinel
    내용
    - redis sentinel 설정법, application.yml

---
+++ HelloSentinel/src/main/java/dev/practice/HelloSentinel/HelloSentinelApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.HelloSentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSentinelApplication.class, args);
+	}
+
+}
---
+++ HelloSentinel/src/test/java/dev/practice/HelloSentinel/HelloSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.HelloSentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
