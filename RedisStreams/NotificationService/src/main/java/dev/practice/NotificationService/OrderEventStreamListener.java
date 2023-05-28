b9f6472a1aab7c9abb177d592c8673a0565e0b57
starryeye <see2818@naver.com>
2023-05-28 22:17:56 +0900

    [starryeye, 2023.05.28] - order-events 이벤트에 대한 notification Listener 구현
    내용
    - order-events 이벤트에 대한 notification Listener 구현

---
+++ RedisStreams/NotificationService/src/main/java/dev/practice/NotificationService/OrderEventStreamListener.java
@@ -0,0 +1,32 @@
+package dev.practice.NotificationService;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.stream.StreamListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * Redis Streams Message Listener
+ */
+@Slf4j
+@Component
+public class OrderEventStreamListener implements StreamListener<String, MapRecord<String, String, String>> {
+
+    @Override
+    public void onMessage(MapRecord<String, String, String> message) {
+
+        log.info("order consumed..");
+
+        Map<String, String> values = message.getValue();
+
+        String userId = values.get("userId");
+        String productId = values.get("productId");
+
+        //주문 건에 대한 메일 발송
+        //...
+
+        log.info("order userId={}, productId={}", userId, productId);
+    }
+}
