d82832b81362979e35c0ec03f1ad2ad0f6d7d4d7
starryeye <see2818@naver.com>
2023-06-05 22:24:30 +0900

    [starryeye, 2023.06.05] - Redisson Client Issue Tracking init
    내용
    - https://github.com/redisson/redisson/issues/5072

---
+++ IssueTracking/src/main/java/dev/practice/IssueTracking/IssueTrackingApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.IssueTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingApplication.class, args);
+	}
+
+}
---
+++ IssueTracking/src/test/java/dev/practice/IssueTracking/IssueTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.IssueTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
