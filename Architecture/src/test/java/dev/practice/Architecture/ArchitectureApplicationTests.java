049211e8cf0ef733b5a9a1e2e304a4d001f19736
starryeye <see2818@naver.com>
2023-06-07 02:17:35 +0900

    [starryeye, 2023.06.07] - Redis 와 Arcitecture init
    내용
    - DataStore, Cache 용도로 사용할 경우 패키징 방법

---
+++ Architecture/src/test/java/dev/practice/Architecture/ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.Architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
