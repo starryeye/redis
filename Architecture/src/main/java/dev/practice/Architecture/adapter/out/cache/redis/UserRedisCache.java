13b2de3fd355867b68ec2d74dfce6c0cba7ba5f0
starryeye <see2818@naver.com>
2023-06-07 22:48:49 +0900

    [starryeye, 2023.06.07] - Cache packaging
    내용
    - Cache packaging

---
+++ Architecture/src/main/java/dev/practice/Architecture/adapter/out/cache/redis/UserRedisCache.java
@@ -0,0 +1,5 @@
+package dev.practice.Architecture.adapter.out.cache.redis;
+
+public interface UserRedisCache {
+    //TODO
+}
---
+++ Architecture/src/main/java/dev/practice/Architecture/adapter/out/cache/redis/UserRedisCacheImpl.java
@@ -0,0 +1,4 @@
+package dev.practice.Architecture.adapter.out.cache.redis;
+
+public class UserRedisCacheImpl implements UserRedisCache{
+}
